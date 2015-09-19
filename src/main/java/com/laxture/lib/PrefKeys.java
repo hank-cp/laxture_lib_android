88ac2fab7099867d68cd59d83865858f38338f22
Hank CP <true.cp@gmail.com>
2015-09-20 00:57:27 +0800

    Change constant to interface

--- src/main/java/com/laxture/lib/PrefKeys.java
+++ src/main/java/com/laxture/lib/PrefKeys.java
@@ -1,9 +1,9 @@
 package com.laxture.lib;
 
-public class PrefKeys {
+public interface PrefKeys {
 
-    public static final String PREF_KEY_APP_TO_BACKGROUND = "app_to_background";
-    public static final String PREF_KEY_APP_TO_BACKGROUND_TIME = "app_to_background_time";
-    public static final String PREF_KEY_DEVICE_ID = "device_id";
+    String PREF_KEY_APP_TO_BACKGROUND = "app_to_background";
+    String PREF_KEY_APP_TO_BACKGROUND_TIME = "app_to_background_time";
+    String PREF_KEY_DEVICE_ID = "device_id";
 
 }
